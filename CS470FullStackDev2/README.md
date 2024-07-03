22de38f40c9caa7dec99e2e615f0508c5e4a81bd
Kyle Dale <kdale.main@gmail.com>
2024-07-03 08:04:45 -0400

    Uploaded files
    Attempt at Uploading Stack Dev II files

---
+++ CS470FullStackDev2/README.md
@@ -0,0 +1,2 @@
+# WorkShowcaseKD
+Showcase of completed projects and work for Kyle Dale
---
+++ CS470FullStackDev2/lafs-web/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api_url: 'https://ho9dr8v574.execute-api.us-east-2.amazonaws.com'
+};
